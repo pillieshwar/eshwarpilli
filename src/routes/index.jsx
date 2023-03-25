0fb4a4f8dddba2551380247aa2cb8bdaa7378200
eshwar <pillieshwar@gmail.com>
2023-03-24 22:02:29 -0700

    Add live projects

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -10,108 +10,134 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import EshwarNagPilli from "./../images/Eshwar_Nag_Pilli.jpeg";
+import Terodoc_Statement_of_Purpose from "./../images/Terodoc_Statement_of_Purpose.png";
 import { Link } from "react-router-dom";
+import Badge from "@mui/material/Badge";
+import SALESTAT_LOGO from "./../images/SALESTAT_LOGO.svg";
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    "&::after": {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
+      content: '""',
+    },
+  },
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
+      opacity: 1,
+    },
+    "100%": {
+      transform: "scale(2.4)",
+      opacity: 0,
+    },
+  },
+}));
 
 export default function Index() {
+  const infoStack = [
+    [
+      Terodoc_Statement_of_Purpose,
+      "Terodoc",
+      "A platform that provides a comprehensive library of Statement of Purpose that have been approved and accepted by universities. It offers a user-friendly interface for accessing and customizing SOPs, ensuring that they meet the specific needs of each institution.",
+    ],
+    [
+      SALESTAT_LOGO,
+      "Salestat",
+      "SaleStat is a software product which helps the medical company to analyse their sales in a graphical view and get a clear picture of their monthly and yearly sales status",
+    ],
+  ];
   return (
-    <p>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container pt={2} spacing={10}>
-          <Grid xs={12} md={4}>
-            <Card sx={{ maxWidth: 420 }}>
-              <CardMedia
-                sx={{ height: 400 }}
-                image={EshwarNagPilli}
-                title="Eshwar Nag Pilli"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                  Eshwar Nag Pilli
-                </Typography>
-                <Typography gutterBottom variant="body2" component="div">
-                  Software Development Engineer(SDE) at Amazon
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Dedicated, hardworking, energetic graduate student studying
-                  Masters in Computer Science at Washington State University.
-                  Skilled in data structures, algorithms, problem-solving, and
-                  web development. Passionate about Innovation.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
+    <Box sx={{ flexGrow: 1 }} pt={10}>
+      <Grid container spacing={5}>
+        <Grid xs={12} md={4}>
+          <Card>
+            <CardMedia
+              sx={{ height: 400 }}
+              image={EshwarNagPilli}
+              title="Eshwar Nag Pilli"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Eshwar Nag Pilli
+              </Typography>
+              <Typography gutterBottom variant="body2" component="div">
+                Software Development Engineer(SDE) at Amazon
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Dedicated, hardworking, energetic graduate student studying
+                Masters in Computer Science at Washington State University.
+                Skilled in data structures, algorithms, problem-solving, and web
+                development. Passionate about Innovation.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid xs={12} md={8}>
+          <Grid container spacing={2}>
+            <Grid xs={12} textAlign="center">
+              <StyledBadge
+                // sx={{
+                //   minWidth: 0,
+                //   ml: open ? 3 : "auto",
+                //   justifyContent: "center",
+                // }}
+                overlap="circular"
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                variant="dot"
+              ></StyledBadge>
+              <Typography
+                pl={1}
+                gutterBottom
+                variant="body2"
+                component="text.secondary"
+              >
+                LIVE PROJECTS
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid xs={12} md={8}>
+          {infoStack.map((data) => (
             <Grid container spacing={2}>
-              <Grid xs={12} md={6}>
+              <Grid xs={12} md={2}>
+                <div class="box">
+                  <img
+                    height={"100vh"}
+                    src={data[0]}
+                    alt="Find Statement of Purpose examples and get your statement of purpose reviewed"
+                  />
+                </div>
+              </Grid>
+              <Grid xs={12} md={10}>
                 <Typography gutterBottom variant="h6" component="div">
                   <Link
                     to="https://terodoc.com/"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    Terodoc
+                    {data[1]}
                   </Link>
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  A platform that provides a comprehensive library of Statement
-                  of Purpose (SOPs) that have been approved and accepted by
-                  universities. It offers a user-friendly interface for
-                  accessing and customizing SOPs, ensuring that they meet the
-                  specific needs of each institution.
-                </Typography>
-              </Grid>
-              <Grid xs={12} md={6}>
-                <Typography gutterBottom variant="h6" component="div">
-                  Salestat
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  SaleStat is a software product which helps the medical company
-                  to analyse their sales in a graphical view and get a clear
-                  picture of their monthly and yearly sales status
-                </Typography>
-              </Grid>
-              <Grid xs={12} md={6}>
-                <Typography gutterBottom variant="h6" component="div">
-                  Terodoc
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  A platform that provides a comprehensive library of Statement
-                  of Purpose (SOPs) that have been approved and accepted by
-                  universities. It offers a user-friendly interface for
-                  accessing and customizing SOPs, ensuring that they meet the
-                  specific needs of each institution.
-                </Typography>
-              </Grid>
-              <Grid xs={12} md={6}>
-                <Typography gutterBottom variant="h6" component="div">
-                  Salestat
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  SaleStat is a software product which helps the medical company
-                  to analyse their sales in a graphical view and get a clear
-                  picture of their monthly and yearly sales status
-                </Typography>
-              </Grid>
-              <Grid xs={12} md={6}>
-                <Typography gutterBottom variant="h6" component="div">
-                  Terodoc
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  A platform that provides a comprehensive library of Statement
-                  of Purpose (SOPs) that have been approved and accepted by
-                  universities. It offers a user-friendly interface for
-                  accessing and customizing SOPs, ensuring that they meet the
-                  specific needs of each institution.
+                  {data[2]}
                 </Typography>
               </Grid>
             </Grid>
-          </Grid>
+          ))}
         </Grid>
-      </Box>
-    </p>
+      </Grid>
+    </Box>
   );
 }
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,60 +1,168 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Unstable_Grid2";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import { Outlet, NavLink, Link } from "react-router-dom";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+const drawerWidth = 240;
+const navItems = ["Projects", "Blogs"];
 
-export default function Root() {
+function Root(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Eshwar Nag Pilli
+      </Typography>
+      <Divider />
+      <List>
+        <ListItem key="homeHam" disablePadding>
+          <ListItemButton sx={{ textAlign: "center" }}>
+            <ListItemText>
+              <Link
+                style={{
+                  textDecoration: "None",
+                  color: "#000",
+                }}
+                to={`/`}
+              >
+                Home
+              </Link>
+            </ListItemText>
+          </ListItemButton>
+        </ListItem>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText>
+                <NavLink
+                  key={item}
+                  style={{
+                    textDecoration: "None",
+                    color: "#000",
+                  }}
+                  to={`/` + item}
+                >
+                  {item}
+                </NavLink>
+              </ListItemText>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <>
-      <div id="detail">
-        <Grid container spacing={2} columns={12}>
-          <Grid xs={4.5}></Grid>
-          <Grid xs={1} sx={{ paddingTop: 1 }}>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar style={{ backgroundColor: "#000" }} component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
             <Link
               style={{
                 textDecoration: "None",
-                color: "black",
+                color: "#fff",
               }}
               to={`/`}
             >
-              Portfolio
+              Eshwar Nag Pilli
             </Link>
-          </Grid>
-          <Grid xs={1} sx={{ paddingTop: 1 }}>
-            <NavLink
-              style={{
-                textDecoration: "None",
-                color: "black",
-              }}
-              to={`/projects`}
-            >
-              Projects
-            </NavLink>
-          </Grid>
-          <Grid xs={1} sx={{ paddingTop: 1 }}>
-            <Link
-              style={{
-                textDecoration: "None",
-                color: "black",
-              }}
-              to={`/blogs`}
-            >
-              Blogs
-            </Link>
-          </Grid>
-        </Grid>
-        <Outlet />
-      </div>
-    </>
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "flex" } }}>
+            <Button key="Home" sx={{ color: "#fff" }}>
+              <Link
+                style={{
+                  textDecoration: "None",
+                  color: "#fff",
+                }}
+                to={`/`}
+              >
+                Home
+              </Link>
+            </Button>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: "#fff" }}>
+                <NavLink
+                  key={item}
+                  style={{
+                    textDecoration: "None",
+                    color: "#fff",
+                  }}
+                  to={`/` + item}
+                >
+                  {item}
+                </NavLink>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Outlet />
+    </Box>
   );
 }
+
+Root.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default Root;
